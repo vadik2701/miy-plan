9c77fe9b6ba3fe86ae20b6fa54875c8a40ef963b
vadik2701 <fandi4vadim@gmail.com>
2026-06-09 10:50:45 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "delivery-board-v3";
+const CACHE = "delivery-board-v4";
 const ASSETS = ["./", "index.html", "manifest.webmanifest", "icon.svg"];
 
 self.addEventListener("install", event => {
